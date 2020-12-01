1cc5c490546c9a646d4be2d0e609d2f2be26d6f8
SantosMaycon <mayconalves.santos14@gmail.com>
2020-12-01 03:09:06 -0300

    applying logic to insert number or operation

--- js/controller/CalculatorController.js
+++ js/controller/CalculatorController.js
@@ -1,7 +1,7 @@
 class CalculatorController {
   constructor() {
     this._locale = 'pt-BR';
-    this._operation = [];
+    this._operation = [0];
     this._display = document.querySelector('#display');
     this._time = document.querySelector('#hora');
     this._date = document.querySelector('#data');
@@ -47,7 +47,7 @@ class CalculatorController {
     this._date.innerHTML = value;
   }
 
-  // calculator logic
+  // adding click event on the buttons
 
   initializeButtons() {
     let buttons = document.querySelectorAll('#buttons > g, #parts > g');
@@ -58,6 +58,7 @@ class CalculatorController {
     eventListeners.forEach( (eventListener) => {
       buttons.forEach((button) =>  button.addEventListener(eventListener, () =>{
         let buttonValue = button.className.baseVal.replace('btn-','');
+        this.optionExecuted(buttonValue);
         console.log(buttonValue);
       }));
     });
@@ -68,4 +69,178 @@ class CalculatorController {
       }));
     })
   }
+
+  // switch case logic
+
+  optionExecuted(value){
+    switch (value) {
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+        this.addOperation("+");
+        break;
+      case 'subtracao':
+        this.addOperation('-');
+        break;
+      case 'multiplicacao':
+        this.addOperation('*');
+        break;
+      case 'divisao':
+        this.addOperation('/');
+        break;
+      case 'porcento':
+        this.addOperation('%');
+        break;
+      case 'ponto':
+        this.addOperation('.');
+        break;
+      
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(+value);
+        break;
+      
+      default:
+        this.setError();
+        break;
+    }
+  }
+
+  clearAll() {
+    this._operation = [0];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  setError() {
+    this.displayCalc = 'Error';
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  concatNumber(value) {
+    this._operation[this._operation.length - 1] = +(this._operation[this._operation.length - 1].toString() + value.toString());
+  }
+  
+  concatString(value) {
+    this._operation[this._operation.length - 1] = this._operation[this._operation.length - 1].toString() + value.toString();
+  }
+
+  
+  addOperation(value) {
+    if(isNaN(value)){
+      if(isNaN(this.getLastOperation())) {
+        console.log("Valor passado é String");
+        console.log("Valor anterior é String");
+        console.log("Vou concatenar o valor anterior")
+        this.concatString(value);
+      } else {
+        console.log("Valor passado é String");
+        console.log("Valor anterior é Number");
+        console.log("Vou utilizar o metodo Push!");
+        this._operation.push(value);
+        console.log(this._operation);
+      }
+    } else {
+      if(isNaN(this.getLastOperation())) {
+        console.log("Valor passado é Number");
+        console.log("Valor anterior é String");
+        console.log("Vou utilizar o metodo Push!");
+        this._operation.push(value);
+        console.log(this._operation);
+      } else {
+        console.log("Valor passado é Number");
+        console.log("Valor anterior é Number");
+        console.log("Vou concatenar o valor anterior");
+        this.concatNumber(value);
+        console.log(this._operation);
+      }
+    }
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // addOperation(value) {
+  //   if(isNaN(value)) {
+  //     // Value is a string
+  //     if(!isNaN(this.getLastOperation)){
+  //       // The last value is number
+  //       this._operation.push(value);
+  //       console.log(this._operation);
+  //     } else {
+  //       // The last value is string
+  //       this.concatString(value);
+  //       console.log(this._operation);
+  //     }
+  //   } else {
+  //     // Value is a number
+  //     if(!isNaN(this.getLastOperation)){
+  //       // The last value is number
+  //       this.concatNumber(value);
+  //       console.log(this._operation);
+  //     } else {
+  //       // The last value is string
+  //       this._operation.push(value);
+  //       console.log(this._operation);
+  //     }
+  //   }
+  // }
