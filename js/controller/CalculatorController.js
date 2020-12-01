e9ed54bb59c1aad049a114bb7016a944d710241b
SantosMaycon <mayconalves.santos14@gmail.com>
2020-12-01 00:35:55 -0300

    adding click event on the buttons

--- js/controller/CalculatorController.js
+++ js/controller/CalculatorController.js
@@ -1,10 +1,12 @@
 class CalculatorController {
   constructor() {
+    this._locale = 'pt-BR';
+    this._operation = [];
     this._display = document.querySelector('#display');
     this._time = document.querySelector('#hora');
     this._date = document.querySelector('#data');
-    this._locale = 'pt-BR';
     this.initializeDisplay();
+    this.initializeButtons();
   }
 
   initializeDisplay() {
@@ -43,5 +45,27 @@ class CalculatorController {
 
   set currentDate(value) {
     this._date.innerHTML = value;
+  }
+
+  // calculator logic
+
+  initializeButtons() {
+    let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+    
+    const eventListeners = ['click', 'drag'];
+    const eventListenersPointers = ['mouseover', 'mouseup', 'mousedown'];
+
+    eventListeners.forEach( (eventListener) => {
+      buttons.forEach((button) =>  button.addEventListener(eventListener, () =>{
+        let buttonValue = button.className.baseVal.replace('btn-','');
+        console.log(buttonValue);
+      }));
+    });
+    
+    eventListenersPointers.forEach( (pointer) => {
+      buttons.forEach((button) =>  button.addEventListener(pointer, () => {
+        button.style.cursor = 'pointer';
+      }));
+    })
   }
 }
